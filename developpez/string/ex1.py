fcb32d223c754baec5e697ce5562a1a08002dfa9
Thai Dang <thai@home>
2022-07-29 00:26:42 +0200

    refactor

---
+++ developpez/string/ex1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+str1 = input("Chaine :")
+
+if 'e' in str1:
+    print(str1 + " contient e")
+else:
+    print(str1 + " ne contient pas e")
---
+++ developpez/string/ex2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+str1 = "gaston"
+#str2 = list(str1)
+#str3 = "*".join(str2)
+#print(str3)
+print("*".join(str1))
