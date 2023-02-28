3b9b1b0a2bf06ab91448a99b87e33fe85bd529bc
Thai DANG <thai.dang.fr@gmail.com>
2023-02-28 21:30:12 +0100

    file io

---
+++ formation/imtiaz/Section_06/application.py
@@ -0,0 +1,25 @@
+def sum(num1, num2):
+    try:
+        print(num1+num2)
+    except:
+        print("error found")
+
+def interactive():        
+    number1 = input("enter a number: ")
+    sum(number1, 12)
+
+
+with open("sample.txt", mode="w+") as myfile:
+    myfile.write("It involves drastically reducing carbohydrate intake and replacing it with fat. This reduction in carbs puts your body into a metabolic state called ketosis.\n\nWhen this happens, your body becomes incredibly efficient at burning fat for energy.")
+
+print("method 1:")
+myfile = open("sample.txt")
+print(myfile.read())
+print("--")
+myfile.seek(0)
+print(myfile.readlines())
+myfile.close()
+
+print("method 2:")
+with open("sample.txt", mode="w+") as myfile:
+    print(myfile.read())
