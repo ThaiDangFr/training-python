a19efd6b5bfce0ac68e3ef24d977b0656bcbab5b
Thai DANG <thai.dang.fr@gmail.com>
2023-03-20 19:02:47 +0100

    poo variables et methods

---
+++ formation/lab/poo_queue.py
@@ -0,0 +1,39 @@
+class QueueError(IndexError):  
+    def __init__(self):
+        IndexError.__init__(self)
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+    
+    def put(self, elem):
+        self.queue.append(elem)
+    
+    def get(self):
+        if len(self.queue) > 0:
+            val = self.queue[0]
+            del self.queue[0]
+            return val
+        else:
+            raise QueueError
+
+class SuperQueue(Queue):
+    def __init__(self):
+        Queue.__init__(self)
+
+    def isempty(self):
+        if len(self.queue) == 0:
+            return True
+        else:
+            return False
+        
+que = SuperQueue()
+que.put(1)
+que.put("dog")
+que.put(False)
+
+for i in range(4):
+    if not que.isempty():
+        print(que.get())
+    else:
+        print("Queue empty")
