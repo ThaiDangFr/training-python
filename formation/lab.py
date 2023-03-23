7099f481c51758d29d995c600740b9b71fe7d87c
Thai DANG <thai.dang.fr@gmail.com>
2023-03-23 18:42:07 +0100

    poo exceptions

--- formation/lab.py
+++ formation/lab.py
@@ -1,37 +1,26 @@
-class Dog:
-    kennel = 0
-    def __init__(self, breed):
-        self.breed = breed
-        Dog.kennel += 1
-    def __str__(self):
-        return self.breed + " says: Woof!"
+class I:
+    def __init__(self):
+        self.s = 'abc'
+        self.i = 0
+ 
+    def __iter__(self):
+        return self
+ 
+    def __next__(self):
+        if self.i == len(self.s):
+            raise StopIteration
+        v = self.s[self.i]
+        self.i += 1
+        return v
  
  
-class SheepDog(Dog):
-    def __str__(self):
-        return super().__str__() + " Don't run away, Little Lamb!"
- 
- 
-class GuardDog(Dog):
-    def __str__(self):
-        return super().__str__() + " Stay where you are, Mister Intruder!"
- 
-class LowlandDog(SheepDog):
-    def __str__(self):
-        return Dog.__str__(self) + " I don't like mountains"
+for x in I():
+    print(x,end='')
 
-    
-rocky = SheepDog("Collie")
-luna = GuardDog("Dobermann")
 
-print(rocky) # Collie says: Woof! Don't run away, Little Lamb!
-print(luna)  # Dobermann says: Woof! Stay where you are, Mister Intruder!
+mylist = [1,2]
+myiter = iter(mylist)
 
-print(issubclass(SheepDog, Dog), issubclass(SheepDog, GuardDog)) # True, False
-print(isinstance(rocky, GuardDog), isinstance(luna, GuardDog))   # False, True
-
-print(luna is luna, rocky is luna) # True, False
-print(rocky.kennel)                # 2
-
-toto = LowlandDog("Toto")
-print(toto) # Toto says: Woof! I don't like mountains
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
