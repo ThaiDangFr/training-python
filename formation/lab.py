97161f3af992e9cf679dc6676e14551fc3735ad1
Thai DANG <thai.dang.fr@gmail.com>
2023-03-30 15:24:55 +0200

    fixes

--- formation/lab.py
+++ formation/lab.py
@@ -1,16 +1,17 @@
 class A:
-    def print(self):
-        print("A")
-
-class B(A):
-    def __init__(self, name):
-        self.name = name
-        print(super)
-        print(super())
-
-    def print(self):
-        print("B")
-
-
-b = B("O")
-b.print()
+    def a(self):
+        print('a')
+ 
+ 
+class B:
+    def a(self):
+        print('b')
+ 
+ 
+class C(B, A):
+    def c(self):
+        self.a()
+ 
+ 
+o = C()
+o.c()
