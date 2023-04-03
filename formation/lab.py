6c194ffe206e92ec16037866093b280501f83a06
Thai DANG <thai.dang.fr@gmail.com>
2023-04-03 20:05:16 +0200

    practice 3

--- formation/lab.py
+++ formation/lab.py
@@ -1,9 +1,21 @@
-
-x = [1,2,3]
-def func():
-    print(x)
-    x.pop()
-    del x[1]
-
-func()
-print(x)
+class A:
+    def __init__(self):
+        #self.i = 1
+        print("constructeur A appelé")
+ 
+    def func(self):
+        self.i = 10
+ 
+ 
+class B(A):
+    #def __init__(self):
+    #    print("constructeur B appelé")
+    def func(self):
+        pass
+        #self.i += 1
+        #return self.i
+ 
+ 
+b = B()
+#print(b.func())
+#b.func()
