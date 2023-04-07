ab393d89d9b9f8947aacc9e1f90edceef68d6924
Thai DANG <thai.dang.fr@gmail.com>
2023-04-07 18:46:10 +0200

    eod

--- formation/lab.py
+++ formation/lab.py
@@ -1,16 +1,10 @@
-marks = [80, 70, 90, 90, 80, 100]
-average = sum(marks) // len(marks)
-grade = ''
+class A:
+    pass
 
-if 90 <= average <= 100:
-    grade = 'A'
-elif 80 <= average < 90:
-    grade = 'B'
-elif 70 <= average < 80:
-    grade = 'C'
-elif 65 <= average < 70:
-    grade = 'D'
-else:
-    grade = 'F'
- 
-print(grade)
+class B(A):
+    def __init__(self):
+        super().__init__()
+
+
+b = B()
+
